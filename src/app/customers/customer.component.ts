7d75b9f936c3df16529421b46091e70675db3627
Olabanji Mayowa Emmanuel <mayowae@outlook.com>
2021-02-13 14:51:19 +0100

    Understand SetValue()

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -27,4 +27,13 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  populateTestData(): void{
+    this.customerForm.setValue({
+      firstName: 'Mayor',
+      lastName: 'Emman',
+      email: 'may@gm.com',
+      sendCatalog: false
+    })
+  }
 }
